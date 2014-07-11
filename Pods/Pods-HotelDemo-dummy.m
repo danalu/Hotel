7b50f53b58b4861ba5a8754400f84db2243a2ad0
guhun <tianranwuwai@yeah.net>
2014-07-11 10:27:03 +0800

    delete

--- Pods/Pods-HotelDemo-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_HotelDemo : NSObject
-@end
-@implementation PodsDummy_Pods_HotelDemo
-@end
--- Pods/Pods-HotelSDK-AFNetworking-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_HotelSDK_AFNetworking : NSObject
-@end
-@implementation PodsDummy_Pods_HotelSDK_AFNetworking
-@end
